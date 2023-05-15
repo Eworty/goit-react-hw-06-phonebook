430a2afe8e946671753f75f66f6896c4178500fc
Pasha Piankovskyi <ghostangel21042017@gmail.com>
2023-05-15 11:02:30 +0300

    add task

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,43 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { defaultContacts } from 'data';
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: { list: defaultContacts },
+  reducers: {
+    add: {
+      reducer(state, action) {
+        state.list.push(action.payload);
+      },
+      prepare(name, number) {
+        return {
+          payload: {
+            name,
+            number,
+            id: nanoid(),
+          },
+        };
+      },
+    },
+
+    remove: (state, action) => {
+      return {
+        list: state.list.filter(contact => contact.id !== action.payload),
+      };
+    },
+  },
+});
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
+
+export const contactsReducer = persistReducer(
+  persistConfig,
+  contactsSlice.reducer
+);
+
+export const { add, remove } = contactsSlice.actions;
